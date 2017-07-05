51daea4241cb73257c20d4a606fd5ce05b22dc65
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-07-05 10:32:08 -0500

    Diff

--- Intermediate Algorithm Scripting/Diff Two Arrays.js
+++ Intermediate Algorithm Scripting/Diff Two Arrays.js
@@ -1,12 +1,15 @@
 
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
-  return newArr;
+  var newArr = arr1.concat(arr2);
+  return newArr.filter(function(value, index, array) {
+    if (array.slice(index+1).indexOf(value) === -1 && array.slice(0, index).indexOf(value) === -1){
+      return value;
+    }
+  });
 }
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
-
+// https://www.youtube.com/watch?v=ylbjD5fHytI
 // use slice to divide array
 // use filter to find the different
 // inside of that, use comparison to find it
