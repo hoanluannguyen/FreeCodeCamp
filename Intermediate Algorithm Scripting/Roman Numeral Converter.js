64ed4a7fbacbab43c46fbc253e37c67c1e6f4c49
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-07-07 10:26:21 -0500

    third

--- Intermediate Algorithm Scripting/Roman Numeral Converter.js
+++ Intermediate Algorithm Scripting/Roman Numeral Converter.js
@@ -1,6 +1,32 @@
+function convertToRoman(num){
+var romans = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
 
-function convertToRoman(num) {
- return num;
+var result = '';
+
+for (var key in romans) {
+  if (num >= romans[key]) {
+    result += key.repeat(Math.trunc(num / romans[key]));
+    //Math.trunc do ground out the odd number,
+    num -= romans[key] * Math.trunc(num / romans[key]);
+  }
 }
 
+return result;
+}
 convertToRoman(36);
+
+//https://www.youtube.com/watch?v=OHIeCYZWVuQ&t=331s
