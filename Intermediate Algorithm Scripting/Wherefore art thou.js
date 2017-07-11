c7b972e62d7708db7b6939c2dddee96ecb78985b
David <hoanluannguyen@gmail.com>
2017-07-10 19:22:52 -0500

    lession 4

--- Intermediate Algorithm Scripting/Wherefore art thou.js
+++ Intermediate Algorithm Scripting/Wherefore art thou.js
@@ -5,8 +5,23 @@ function whatIsInAName(collection, source) {
   // Only change code below this line
 
 
+var srcKeys = Object.keys(source);
+return collection.filter(function(obj) {
+    //"collection" give the value of "key"
+    //for example, Object.keys(collection[0]) give the first object
+    //{ first: "Romeo", last: "Montague" }
+    //of the whatIsInAName array
+    //it will show all the key of that Object. FIRST & LAST
+    return srcKeys.every(function(key) {
+        //"every" return to true/false value
+        //if every item in array pass the test, it return true,
+        //if just 1 item in array isn't pass, it return false,
+        return obj.hasOwnProperty(key) && obj[key] === source [key];
+    });
+});
+
   // Only change code above this line
-  return arr;
+ //return arr;
 }
 
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
