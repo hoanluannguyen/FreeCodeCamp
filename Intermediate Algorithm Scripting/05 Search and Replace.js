c828139209391487588d4c15d6537189ee2af5b9
David <hoanluannguyen@gmail.com>
2017-07-10 20:06:28 -0500

    lesson 5

---
+++ Intermediate Algorithm Scripting/05 Search and Replace.js
@@ -0,0 +1,10 @@
+function myReplace(str, before, after) {
+    if(before.charAt(0) === before.charAt(0).toUpperCase()) {
+        after = after.charAt(0).toUpperCase() + after.slice(1);
+    }
+    //to make UpperCase in "after" word
+
+  return str.replace(before,after);
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
