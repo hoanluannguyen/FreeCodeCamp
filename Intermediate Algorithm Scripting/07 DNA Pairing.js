4e12e77653a833613bd4107abc30512ead5c5503
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-07-11 15:29:11 -0500

    lesson 7

--- Intermediate Algorithm Scripting/07 DNA Pairing.js
+++ Intermediate Algorithm Scripting/07 DNA Pairing.js
@@ -1,5 +1,13 @@
 function pairElement(str) {
-  return str;
+  var pairs = {A: 'T', T: 'A', C: 'G', G: 'C'};
+  // 4 keys with 4 element
+  return str.split('').map(function(item) {
+    return [item, pairs[item]];
+    // taking out the string and splitting it up to arrays
+    // using bracket notation
+    // using dot not work:   pairs.item   because there are no key in this object called item
+    // so it will give "null" value
+  });
 }
 
 
