d54b32ccbea0aa32c60f4ee94a3d07fcd288522a
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-07-11 16:15:24 -0500

    less 9, really short

---
+++ Intermediate Algorithm Scripting/09 Boo Who.js
@@ -0,0 +1,13 @@
+
+function booWho(bool) {
+  if (bool === true || bool === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//we can do simplier way:
+//return bool --- true || bool false;
+
+console.log(booWho(null));
