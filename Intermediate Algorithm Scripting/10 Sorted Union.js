b0aab6693f0549a0bb6f5cf3fed572a161a60aba
David <hoanluannguyen@gmail.com>
2017-07-12 23:20:11 -0500

    lession 10

--- Intermediate Algorithm Scripting/10 Sorted Union.js
+++ Intermediate Algorithm Scripting/10 Sorted Union.js
@@ -1,6 +1,15 @@
 
 function uniteUnique(arr) {
-  return arr;
+  var args = Array.prototype.slice.call(arguments);
+  var result = [];
+  args.reduce(function(a,b) {
+      return a.concat(b);
+  }).forEach(function(item) {
+     if (result.indexOf(item) < 0) {
+         result.push(item);
+     }
+ });
+ return result;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
