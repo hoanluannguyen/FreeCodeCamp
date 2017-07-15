5842d0a83b2c3825c0c45fb36b7c816d961a82ab
David <hoanluannguyen@gmail.com>
2017-07-15 11:24:48 -0500

    lession 12

--- Intermediate Algorithm Scripting/12 Spinal Tap Case.js
+++ Intermediate Algorithm Scripting/12 Spinal Tap Case.js
@@ -1,8 +1,13 @@
 
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  return str;
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase();
+  //betwwen '$1 $2' is the space, if we put any character between them,
+  // for example "..." the result will be "this...is...Spinal...case"
+  return str.replace(/\s|_/g, '-'); 
+  // "|" mean or
+  // "s" mean space?
+  // "g" mean global
+  // study again expression
 }
 
 spinalCase('This Is Spinal Tap');
