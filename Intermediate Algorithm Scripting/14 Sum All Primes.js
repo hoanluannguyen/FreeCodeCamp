150ac29e28a13a2511b5e7246e074927dcd6abdd
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-08-09 09:29:47 -0500

    lesson 14

--- Intermediate Algorithm Scripting/14 Sum All Primes.js
+++ Intermediate Algorithm Scripting/14 Sum All Primes.js
@@ -1,6 +1,25 @@
 
 function sumPrimes(num) {
-  return num;
+  var range = [];
+  for (var i = 2; i <= num; i++) {
+    if (isPrimeNumber(i)) {
+      range.push(i);
+    }
+  }
+  return range.reduce(function(a, b) {
+    return a + b;
+  });
+}
+// dont put everything in only function
+// break it up to several funcion, easier to debug and use next time
+
+function isPrimeNumber(num) {
+  for (var y = 2; y < num; y++) {
+    if (num % y === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 sumPrimes(10);
