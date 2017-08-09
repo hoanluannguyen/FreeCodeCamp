407cfb61b49d0876e23b7fa17cff380b384e8052
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-08-09 08:53:33 -0500

    lesson 13

--- Intermediate Algorithm Scripting/13 Sum All Odd Fobinacci Numbers.js
+++ Intermediate Algorithm Scripting/13 Sum All Odd Fobinacci Numbers.js
@@ -1,6 +1,25 @@
+//study more about sequence
+
+Array.prototype.last = function() {
+  return this[this.length-1];
+};
+
+Array.prototype.secondToLast = function() {
+  return this[this.length-2];
+};
 
 function sumFibs(num) {
-  return num;
+  var sequence = [1,1];
+
+  while (sequence.secondToLast() + sequence.last() <= num) {
+    sequence.push(sequence.secondToLast() + sequence.last());
+  }
+
+  return sequence.filter(function(num) {
+    return num % 2!== 0;
+  }).reduce(function(a,b) {
+    return a + b;
+  });
 }
 
 sumFibs(4);
