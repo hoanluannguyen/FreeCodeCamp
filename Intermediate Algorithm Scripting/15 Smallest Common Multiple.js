746d60300aae5b8ecc6d16e9c854fcc6341f7384
Luan Nguyen <hoanluannguyen@yahoo.com.vn>
2017-08-09 09:51:49 -0500

    lesson 15

--- Intermediate Algorithm Scripting/15 Smallest Common Multiple.js
+++ Intermediate Algorithm Scripting/15 Smallest Common Multiple.js
@@ -1,7 +1,29 @@
 
 function smallestCommons(arr) {
-  return arr;
-}
+  var min = Math.min(arr[0], arr[1]);
+  var max = Math.max(arr[0], arr[1]);
+  var range = [];
 
+  for (var x = min; x <= max; x++) {
+    range.push(x);
+  }
+
+var a = Math.abs(range[0]);
+
+for (var i = 1; i < range.length; i++) {
+  var b = Math.abs(range[i]);
+  var c = a;
+
+  while (a && b) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
+    }
+    }
+    a = Math.abs(c * range[i] / (a + b));
+  }
+  return a;
+}
 
 smallestCommons([1,5]);
